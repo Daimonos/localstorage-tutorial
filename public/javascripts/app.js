ad5c0cc6ecbff1aaa9683aa61a417c443663a0ec
James <jim.d.hughes@gmail.com>
2015-04-16 23:26:07 -0600

    Tutorial Part 2

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -12,6 +12,24 @@ app.config(['$routeProvider', '$locationProvider', '$httpProvider',
     })
 }]);
 
-app.controller('MainCtrl', ['$scope', function($scope){
-	$scope.message = 'What\'s up AngularJS?';
+app.controller('MainCtrl', ['$scope','$http', function($scope, $http){
+	$scope.message = 'Check out this inventory';
+  //get us some datas
+  $http.get('/inventory')
+  .success(function(data, status, headers, config){
+    $scope.inventory = data;
+  })
+  .error(function(data, status, headers, config){
+    $scope.message = 'Something exploded! '+data.message;
+  });
+
+  //scope helper functions
+  $scope.edit = function(index){
+    $scope.editing = true;
+    $scope.item = $scope.inventory[index];
+  }
+  $scope.finishEditing = function(){
+    $scope.editing = false;
+    $scope.item = {};
+  }
 }]);
